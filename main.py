0beb982491b804c75556df661e6beea9e03eb8b7
lanaHamayel5 <lana12.amjad@gmail.com>
2024-08-01 12:33:50 +0300

    reduse duplication

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 # Task 1: Simple Quiz implementaion
 import json
  
-values = [4, 1, 1, 1, 3, 4, 1]
- 
 score = 0
-index = 0
- 
+
 # Functions prototype
 def check_answer(entered_answer, correct_answer):
-    global score,index
-   
-    # print(f"index = {index}")
-    if int(entered_answer) == values[index]:        
+    global score
+    if int(entered_answer) == correct_answer:        
         score += 1  # Increase score for a correct answer
-        index+=1
         return "correct"
        
     else:
-        index+=1
         return "wrong"
    
  
@@ -38,53 +31,33 @@ def main():
     # Reading file  
     with open('data.json', 'r') as file:
         data = json.load(file)
-    # to make sure that the data was read correctlly
-    # print(data)
- 
-    j=1
+    
+    num_of_options=1
     questions_count = 0
-    #Part1: Sport Questions
-    print("Sport Questions:")
-    for q_key, q_value in data['quiz']['sport'].items():
-        print(f"Question: {q_value['question']}\n")
-       
-        print("Options:")
-        for option in q_value['options']:
-            print(f"{j})  {option}")
-            j+=1
-        j=1
-        print()  # Adds a newline for separation
-       
-        choice = get_valid_choice()
-       
-        result = check_answer(choice, q_value['answer'])
-        print(f"Your answer is {result}\n")
-        print()  # Adds an extra newline for separation
-        questions_count+=1
-       
-       
-       
-    # Part2: Math Questions
-    print("Math Questions:")
-    for q_key, q_value in data['quiz']['maths'].items():
-        print(f"Question: {q_value['question']}\n")
-       
-        print("Options:")
-        for option in q_value['options']:
-            print(f"{j}) {option}")
-            j+=1
-        j=1
-        print()
-       
-        choice = get_valid_choice()
-       
-        result = check_answer(choice, q_value['answer'])
-        print(f"Your answer is {result}\n")
-        print()  
-        questions_count+=1
    
+    for question_type in ['sport', 'maths']:
+        print(f"{question_type.capitalize()} Questions:")
+        for q_key, q_value in data['quiz'][question_type].items():
+            print(f"Question: {q_value['question']}\n")
+            
+            print("Options:")
+            for option in q_value['options']:
+                print(f"{num_of_options})  {option}")
+                num_of_options+=1
+            num_of_options=1
+            print()  # Adds a newline for separation
+            
+            choice = get_valid_choice()      
+            result = check_answer(choice, (q_value['options'].index(q_value['answer']))+1)
+            print(f"Your answer is {result}\n")
+            print()  # Adds an extra newline for separation
+            questions_count+=1
+        
     #Present the final answer  
     print(f"Your final score = {score}/{questions_count}  ")
  
 if __name__ == "__main__":
     main()
+    
+    
+    
